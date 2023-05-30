42e60cb56f83bcca42ddcc93bfe5a75b308b59a8
Stepan72 <st.nim@list.ru>
2023-05-30 22:44:08 +0600

    setup completed v0.01

--- app/page.js
+++ app/page.js
@@ -1,9 +1,47 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
+import { FiSettings } from "react-icons/fi";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+import { Navbar, Footer, Sidebar, ThemeSettings } from "@components";
 
 function Home() {
+  const activeMenu = false;
+
   return (
     <div>
-      <h2 className="underline text-xl text-center text-bold">Home Check</h2>
+      <div className="flex relative dark:bg-main-dark-bg ">
+        <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
+          <TooltipComponent content="Settings" position="Top">
+            <button
+              type="button"
+              className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
+              style={{ background: "blue", borderRadius: "50%" }}
+            >
+              <FiSettings />
+            </button>
+          </TooltipComponent>
+        </div>
+        {activeMenu ? (
+          <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
+            <Sidebar />
+          </div>
+        ) : (
+          <div className="w-0 dark:bg-secondary-dark-bg">
+            <Sidebar />
+          </div>
+        )}
+        <div
+          className={`${
+            activeMenu ? "md:ml-72" : "flex-2"
+          } dark:bg-main-bg bg-main-bg min-h-screen w-full
+          `}
+        >
+          <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
+            <Navbar />
+          </div>
+        </div>
+        {"<Ecommerce />"}
+      </div>
     </div>
   );
 }
