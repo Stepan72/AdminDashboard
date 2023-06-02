7cbc776aaee47df6d481b1e01c27b32a337ff0be
Stepan72 <st.nim@list.ru>
2023-06-02 14:23:50 +0600

    NavButton start v0.07

---
+++ components/NavButton.jsx
@@ -0,0 +1,9 @@
+"use client";
+import React from "react";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+
+function NavButton({ title, customFunc, icon, color, dotColor }) {
+  return <div>NavButton</div>;
+}
+
+export default NavButton;
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+"use client";
+import React, { useContext, useEffect, useState } from "react";
+import { AiOutlineMenu } from "react-icons/ai";
+import { FiShoppingCart } from "react-icons/fi";
+import { BsChatLeft } from "react-icons/bs";
+import { RiNotification3Line } from "react-icons/ri";
+import { MdKeyboardArrowDown } from "react-icons/md";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+import avatar from "./../public/assets/avatar.jpg";
+import { Cart, Chat, Notification, UserProfile } from ".";
+import StateContext from "@context/state-context";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  const { activeMenu, setActiveMenu } = useContext(StateContext);
+
+  return (
+    <div className="flex justify-between p-2 md:mx-6 relative">Navbar</div>
+  );
 }
 
 export default Navbar;
