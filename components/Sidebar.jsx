8fed9a906b39aae0d2c0083f3dc9a2011180b0be
Stepan72 <st.nim@list.ru>
2023-06-03 13:01:27 +0600

    NavBar completed v0.1

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -7,7 +7,7 @@ import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 import { links } from "@public/assets/dummy";
 
 function Sidebar() {
-  const { activeMenu, setActiveMenu } = useContext(StateContext);
+  const { activeMenu, setActiveMenu, screenSize } = useContext(StateContext);
 
   //// Нужно будет через useState сделать активный линк через хендлер
   const activeLinkState = false;
@@ -17,6 +17,12 @@ function Sidebar() {
   const normalLink =
     "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2";
 
+  function handleCloseSideBar() {
+    if (activeMenu && screenSize <= 900) {
+      setActiveMenu(false);
+    }
+  }
+
   return (
     <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
       {activeMenu && (
@@ -25,7 +31,7 @@ function Sidebar() {
             <Link
               href="/"
               onClick={() => {
-                setActiveMenu(false);
+                handleCloseSideBar();
               }}
               className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight text-slate-900 dark:text-white "
             >
@@ -56,7 +62,9 @@ function Sidebar() {
                       <Link
                         href={`/${link.name}`}
                         key={link.name}
-                        onClick={() => {}}
+                        onClick={() => {
+                          handleCloseSideBar();
+                        }}
                         className={`${
                           activeLinkState ? activeLink : normalLink
                         }`}
