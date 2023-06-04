53d51cee892e7a88ac989718d7b686e0215249f2
Stepan72 <st.nim@list.ru>
2023-06-04 18:05:33 +0600

    sparkline middle v0.24

--- components/Charts/SparkLine.jsx
+++ components/Charts/SparkLine.jsx
@@ -1,7 +1,29 @@
+"use client";
 import React from "react";
+import {
+  SparklineComponent,
+  Inject,
+  SparklineTooltip,
+} from "@syncfusion/ej2-react-charts";
 
-function SparkLine() {
-  return <div>SparkLine</div>;
+function SparkLine({ currentColor, id, type, height, width, data, color }) {
+  return (
+    <SparklineComponent
+      id={id}
+      height={height}
+      width={width}
+      valueType="Numeric"
+      lineWidth={1}
+      fill={color}
+      border={{ color: currentColor, width: 2 }}
+      dataSource={data}
+      xName="x"
+      yName="y"
+      type={type}
+    >
+      <Inject services={[SparklineTooltip]} />
+    </SparklineComponent>
+  );
 }
 
 export default SparkLine;
