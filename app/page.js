98bf3ce0c6ebb8d929b2763c100815ab9498c65b
Stepan72 <st.nim@list.ru>
2023-06-04 16:55:40 +0600

    revenue section start v0.22

--- app/page.js
+++ app/page.js
@@ -17,8 +17,8 @@ function Ecommerce() {
 
   return (
     <div className="mt-12">
-      <div className="flex flex-wrap lg:flex-nowrap justify-center">
-        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center">
+      <div className="flex flex-wrap lg:flex-nowrap justify-center  ">
+        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-96 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center">
           <div className="flex justify-between items-center">
             <div>
               <p className="font-bold text-gray-400">Earnings</p>
@@ -26,15 +26,48 @@ function Ecommerce() {
             </div>
           </div>
           <div className="mt-6">
-            {/* <Button
+            <Button
               color="white"
               bgColor="blue"
               text="Download"
               borderRadius="10px"
-            /> */}
+              size="md"
+            />
           </div>
         </div>
+        <div className="flex m-3 flex-wrap justify-center gap-2 items-center">
+          {earningData.map((el) => {
+            return (
+              <div
+                key={el.title}
+                className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56 p-4 pt-9 rounded-2xl"
+              >
+                <button
+                  type="button"
+                  style={{ color: el.iconColor, backgroundColor: el.iconBg }}
+                  className="text-2xl opacity-0.9 rounded-full p-4 hover:drop-shadow-xl"
+                >
+                  {el.icon}
+                </button>
+                <p className="mt-3">
+                  <span className="text-lg font-semibold">{el.amount}</span>
+                  <span
+                    className={`text-sm ${
+                      +el.percentage.split("%")[0] < 0
+                        ? "text-red-600"
+                        : "text-green-600"
+                    } ml-2`}
+                  >
+                    {el.percentage}
+                  </span>
+                </p>
+                <p className="text-sm text-gray-400 mt-1">{el.title}</p>
+              </div>
+            );
+          })}
+        </div>
       </div>
+      <div></div>
     </div>
   );
 }
