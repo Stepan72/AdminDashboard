4cb0c367f6d2ef6fee92db6c34b44cf0d7f39383
Stepan72 <st.nim@list.ru>
2023-06-05 16:56:56 +0600

    Order page middle v0.33

--- app/orders/page.jsx
+++ app/orders/page.jsx
@@ -20,9 +20,40 @@ import { Header } from "@components";
 function Orders() {
   return (
     <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+      PAGE IN WORK!!!
       <Header category="Page" title="Orders" />
+      <GridComponent
+        id="gridcomp"
+        dataSource={ordersData}
+        allowPaging
+        allowSorting
+        allowExcelExport
+        allowPdfExport
+        contextMenuItems={contextMenuItems}
+        // editSettings={editing}
+      >
+        <ColumnsDirective>
+          {ordersGrid.map((el, index) => {
+            <ColumnDirective key={index} {...el} />;
+          })}
+        </ColumnsDirective>
+        <Inject
+          services={[
+            Resize,
+            Sort,
+            ContextMenu,
+            Filter,
+            Page,
+            ExcelExport,
+            Edit,
+            PdfExport,
+          ]}
+        />
+      </GridComponent>
     </div>
   );
 }
 
 export default Orders;
+
+/// 2:22:27
