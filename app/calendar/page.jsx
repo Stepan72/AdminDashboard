45674f1d289cc687d0a140b0d7fe0f2a17ab1c88
Stepan72 <st.nim@list.ru>
2023-06-07 20:09:19 +0600

    Calendar completed v0.5

--- app/calendar/page.jsx
+++ app/calendar/page.jsx
@@ -1,7 +1,37 @@
+"use client";
 import React from "react";
+import {
+  ScheduleComponent,
+  ViewsDirective,
+  ViewDirective,
+  Day,
+  Month,
+  Week,
+  WorkWeek,
+  Agenda,
+  Inject,
+  Resize,
+  DragAndDrop,
+} from "@syncfusion/ej2-react-schedule";
+import { DatePickerComponent } from "@syncfusion/ej2-react-calendars";
+import { scheduleData } from "@public/assets/dummy";
+import { Header } from "@components";
 
 function Calendar() {
-  return <div>Calendar</div>;
+  return (
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="App" title="Calendar" />
+      <ScheduleComponent
+        height="650px"
+        eventSettings={{ dataSource: scheduleData }}
+        selectedDate={new Date(2023, 6, 10)}
+      >
+        <Inject
+          services={[Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop]}
+        />
+      </ScheduleComponent>
+    </div>
+  );
 }
 
 export default Calendar;
