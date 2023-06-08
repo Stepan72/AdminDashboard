b575971f65edb74463ad8e06da76ab69e694a6d1
Stepan72 <st.nim@list.ru>
2023-06-08 08:57:57 +0600

    Editor completed v0.6

--- app/editor/page.jsx
+++ app/editor/page.jsx
@@ -1,7 +1,27 @@
+"use client";
 import React from "react";
+import {
+  HtmlEditor,
+  Image,
+  Inject,
+  Link,
+  QuickToolbar,
+  RichTextEditorComponent,
+  Toolbar,
+} from "@syncfusion/ej2-react-richtexteditor";
+import { EditorData } from "@public/assets/dummy";
+import { Header } from "@components";
 
 function Editor() {
-  return <div>Editor</div>;
+  return (
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="App" title="Editor" />
+      <RichTextEditorComponent>
+        <EditorData />
+        <Inject services={[HtmlEditor, Toolbar, Image, Link, QuickToolbar]} />
+      </RichTextEditorComponent>
+    </div>
+  );
 }
 
 export default Editor;
