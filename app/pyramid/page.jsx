6f4e012dd31fa71fad9265351af155c424374f1a
Stepan72 <st.nim@list.ru>
2023-06-16 21:21:28 +0600

    pyramid completed v0.84

--- app/pyramid/page.jsx
+++ app/pyramid/page.jsx
@@ -1,11 +1,69 @@
 "use client";
-import React from "react";
+import React, { useContext } from "react";
+import {
+  AccumulationChartComponent,
+  AccumulationSeriesCollectionDirective,
+  AccumulationSeriesDirective,
+  Inject,
+  AccumulationLegend,
+  AccumulationDataLabel,
+  AccumulationTooltip,
+  PyramidSeries,
+  AccumulationSelection,
+} from "@syncfusion/ej2-react-charts";
+import { PyramidData } from "@public/assets/dummy";
+import StateContext from "@context/state-context";
+import { Header } from "@components";
 
 function Pyramid() {
-  return <div>Pyramid</div>;
+  const { currentMode } = useContext(StateContext);
+
+  return (
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+      <Header category="Pyramid" title="Food Comparison Chart" />
+      <div className="w-full">
+        <AccumulationChartComponent
+          id="pyramid-chart"
+          legendSettings={{
+            background: currentMode === "Light" ? "white" : "gray",
+            textStyle: { size: "18px" },
+          }}
+          tooltip={{ enable: true }}
+          background={currentMode === "Dark" ? "#33373E" : "#fff"}
+        >
+          <Inject
+            services={[
+              AccumulationDataLabel,
+              AccumulationTooltip,
+              PyramidSeries,
+              AccumulationLegend,
+              AccumulationSelection,
+            ]}
+          />
+          <AccumulationSeriesCollectionDirective>
+            <AccumulationSeriesDirective
+              name="Food"
+              dataSource={PyramidData}
+              xName="x"
+              yName="y"
+              type="Pyramid"
+              width="45%"
+              height="80%"
+              neckWidth="15%"
+              gapRatio={0.03}
+              explode
+              emptyPointSettings={{ mode: "Drop", fill: "red" }}
+              dataLabel={{
+                visible: true,
+                position: "Inside",
+                name: "text",
+              }}
+            />
+          </AccumulationSeriesCollectionDirective>
+        </AccumulationChartComponent>
+      </div>
+    </div>
+  );
 }
 
 export default Pyramid;
-
-/// start for pyramid
-/// initiated
