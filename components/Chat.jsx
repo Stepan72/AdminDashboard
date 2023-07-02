20e5b0f2c916c886e2c5d27acf34ad926b04ffdf
Stepan72 <st.nim@list.ru>
2023-07-02 13:19:31 +0600

    notification finished

--- components/Chat.jsx
+++ components/Chat.jsx
@@ -14,7 +14,7 @@ function Chat() {
           <p className="font-semibold text-lg dark:text-gray-200">Messages</p>
           <button
             type="button"
-            className="text-white text-xs rounded p-1 px-2 bg-orange"
+            className="text-white text-xs rounded p-1 px-2 bg-orange-300 "
           >
             5 New
           </button>
--- components/Notification.jsx
+++ components/Notification.jsx
@@ -1,8 +1,73 @@
 "use client";
-import React from "react";
+import React, { useContext } from "react";
+import { MdOutlineCancel } from "react-icons/md";
+import { Button } from "./index";
+import { chatData } from "@public/assets/dummy";
+import StateContext from "@context/state-context";
 
 function Notification() {
-  return <div>Notif</div>;
+  const { currentColor, handleCancelClick } = useContext(StateContext);
+
+  return (
+    <div className="nav-item absolute right-5 md:right-40 top-16 bg-white dark:bg-[#42464D] p-8 rounded-lg w-96 border">
+      <div className="flex justify-between items-center">
+        <div className="flex gap-3">
+          <p className="font-semibold text-lg dark:text-gray-200">
+            Notifications
+          </p>
+          <button
+            type="button"
+            className="text-white text-xs rounded p-1 px-2 bg-orange-300 "
+          >
+            5 New
+          </button>
+        </div>
+        <Button
+          text={<MdOutlineCancel />}
+          color="rgb(153, 171, 180)"
+          bgHoverColor="light-gray"
+          size="2xl"
+          borderRadius="50%"
+          onClick={() => {
+            handleCancelClick();
+          }}
+        />
+      </div>
+      <div className="mt-5">
+        {chatData.map((el, index) => {
+          return (
+            <div
+              key={index}
+              className="flex items-center leading-8 gap-5 border-b-1 border-color p-3"
+            >
+              <img
+                className="rounded-full h-10 w-10"
+                src={el.image}
+                alt={el.message}
+              />
+              <div>
+                <p className="font-semibold dark:text-gray-200">{el.message}</p>
+                <p className="text-gray-500 text-sm dark:text-gray-400">
+                  {el.desc}
+                </p>
+              </div>
+            </div>
+          );
+        })}
+        <div className="mt-5 flex justify-center">
+          <Button
+            color="white"
+            bgColor={currentColor}
+            text="See all notifications"
+            borderRadius="10px"
+            width="full"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Notification;
+
+//// Notification start ///
